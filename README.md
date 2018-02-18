5d33d233a8beb527a4442a3619bc85c24601b076
Sergey Kovalchuk <sergeyalexkoval@gmaill.com>
2018-02-18 21:30:05 +0300

    money-exchange final version

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### Description
 
 Write a method makeExchange that will determine the minimum number of coins needed to make change for a given amount in American currency.
-Coins used will be half-dollars, quarters, dimes, nickels, and pennies, worth 50¢, 25¢, 10¢, 5¢ and 2¢, respectively.
+Coins used will be half-dollars, quarters, dimes, nickels, and pennies, worth 50¢, 25¢, 10¢, 5¢ and 1¢, respectively.
 They'll be represented by the strings H, Q, D, N and P.
 
 The argument passed in will be an integer representing the value in cents.
@@ -12,19 +12,19 @@ Coins that are not used should not be included in the object.
 
 ### IMPORTANT NOTES
 
-If the argument passed in is 0, then the method should return an empty object.
-If the currency can't be exchanged using 50¢, 25¢, 10¢, 5¢ and 2¢ coins please return next object:
+If the argument passed in is 0 or less, then the method should return an empty object.
+If the currency that you would like to exchange more than 10000 please return next object:
 ```javascript
-   {error: "Sorry! We don't have coins for exchange"}
+   {error: "You are rich, my friend! We don't have so much coins for exchange"}
 ```
 
 ### For example:
 ```javascript
   makeExchange(0)  --> {}
-  makeExchange(1)  --> {error: "Sorry! We don't have coins for exchange"}
-  makeExchange(2)  --> {"P":1}
-  makeExchange(44) --> {"Q":1,"D":1,"N":1,"P":2}
-  makeExchange(92) --> {"H":1,"Q":1,"D":1,"N":1,"P":1}
+  makeExchange(1)  --> {"P":1}
+  makeExchange(43) --> {"Q":1,"D":1,"N":1,"P":3}
+  makeExchange(91) --> {"H":1,"Q":1,"D":1,"N":1,"P":1}
+  makeExchange(9999999)  --> {error: "You are rich, my friend! We don't have so much coins for exchange"}
 ```
 ![Money Exchange](./images/money-exchange.jpg)
 
--- test.js
+++ test.js
@@ -2,11 +2,13 @@ const assert = require('assert');
 const makeExchange = require('./src/index.js');
 
 describe('makeExchange function', () => {
-  it('should return ERROR object if currency can\'t be exchanged', () => {
-    assert.deepEqual(makeExchange(1), {error: "Sorry! We don't have coins for exchange"});
-    assert.deepEqual(makeExchange(16), {error: "Sorry! We don't have coins for exchange"});
-    assert.deepEqual(makeExchange(26), {error: "Sorry! We don't have coins for exchange"});
-    assert.deepEqual(makeExchange(1001), {error: "Sorry! We don't have coins for exchange"});
+  it('should return ERROR object if the currency more than 10000', () => {
+    assert.deepEqual(makeExchange(10001), {error: "You are rich, my friend! We don't have so much coins for exchange"});
+  });
+
+  it('should return correct object if the currency less than or equal to 1000', () => {
+    assert.deepEqual(makeExchange(1000), {"H": 20});
+    assert.deepEqual(makeExchange(9999), {"H":199,"Q":1,"D":2,"P":4});
   });
 
   it('should return empty object if passed currency in is 0', () => {
@@ -16,52 +18,52 @@ describe('makeExchange function', () => {
   it('should return amount of half-dollars correctly', () => {
     assert.deepEqual(makeExchange(50), {"H":1});
     assert.deepEqual(makeExchange(500), {"H":10});
-    assert.deepEqual(makeExchange(102), {"H":2,"P":1});
+    assert.deepEqual(makeExchange(102), {"H":2,"P":2});
   });
 
   it('should return amount of half-dollars correctly', () => {
     assert.deepEqual(makeExchange(50), {"H":1});
     assert.deepEqual(makeExchange(500), {"H":10});
-    assert.deepEqual(makeExchange(102), {"H":2,"P":1});
+    assert.deepEqual(makeExchange(102), {"H":2,"P":2});
   });
 
   it('should return amount of quarters correctly', () => {
     assert.deepEqual(makeExchange(25), {"Q":1});
     assert.deepEqual(makeExchange(75), {"H":1,"Q":1});
-    assert.deepEqual(makeExchange(27), {"Q":1,"P":1});
+    assert.deepEqual(makeExchange(26), {"Q":1,"P":1});
   });
 
   it('should return amount of dimes correctly', () => {
     assert.deepEqual(makeExchange(10), {"D":1});
     assert.deepEqual(makeExchange(35), {"Q":1,"D":1});
-    assert.deepEqual(makeExchange(12), {"D":1,"P":1});
+    assert.deepEqual(makeExchange(11), {"D":1,"P":1});
   });
 
   it('should return amount of nickels correctly', () => {
     assert.deepEqual(makeExchange(5), {"N":1});
     assert.deepEqual(makeExchange(15), {"D":1,"N":1});
-    assert.deepEqual(makeExchange(7), {"N":1,"P":1});
+    assert.deepEqual(makeExchange(7), {"N":1,"P":2});
   });
 
   it('should return amount of pennies correctly', () => {
-    assert.deepEqual(makeExchange(2), {"P":1});
-    assert.deepEqual(makeExchange(12), {"D":1,"P":1});
-    assert.deepEqual(makeExchange(4), {"P":2});
+    assert.deepEqual(makeExchange(1), {"P":1});
+    assert.deepEqual(makeExchange(12), {"D":1,"P":2});
+    assert.deepEqual(makeExchange(4), {"P":4});
   });
 
   it('should return object with exchanged money correctly', () => {
-    assert.deepEqual(makeExchange(44), {"Q":1,"D":1,"N":1,"P":2});
-    assert.deepEqual(makeExchange(92), {"H":1,"Q":1,"D":1,"N":1,"P":1});
+    assert.deepEqual(makeExchange(44), {"Q":1,"D":1,"N":1,"P":4});
+    assert.deepEqual(makeExchange(92), {"H":1,"Q":1,"D":1,"N":1,"P":2});
   });
 
   it('should return amount of trapped water correctly 1', () => {
-    assert.deepEqual(makeExchange(142), {"H":2,"Q":1,"D":1,"N":1,"P":1});
-    assert.deepEqual(makeExchange(144), {"H":2,"Q":1,"D":1,"N":1,"P":2});
+    assert.deepEqual(makeExchange(142), {"H":2,"Q":1,"D":1,"N":1,"P":2});
+    assert.deepEqual(makeExchange(149), {"H":2,"Q":1,"D":2,"P":4});
   });
 
   it('should return amount of trapped water correctly 2', () => {
-    assert.deepEqual(makeExchange(147), {"H":2,"Q":1,"D":2,"P":1});
-    assert.deepEqual(makeExchange(52), {"H":1,"P":1});
+    assert.deepEqual(makeExchange(147), {"H":2,"Q":1,"D":2,"P":2});
+    assert.deepEqual(makeExchange(52), {"H":1,"P":2});
   });
 
   it('should return amount of trapped water correctly 3', () => {
@@ -70,7 +72,31 @@ describe('makeExchange function', () => {
   });
 
   it('should return amount of trapped water correctly 4', () => {
-    assert.deepEqual(makeExchange(62), {"H":1,"D":1,"P":1});
+    assert.deepEqual(makeExchange(62), {"H":1,"D":1,"P":2});
     assert.deepEqual(makeExchange(65), {"H":1,"D":1,"N":1});
+  });
+
+  it('should return amount of trapped water correctly 5', () => {
+    assert.deepEqual(makeExchange(478), {"H":9,"Q":1,"P":3});
+  });
+
+  it('should return amount of trapped water correctly 6', () => {
+    assert.deepEqual(makeExchange(967), {"H":19,"D":1,"N":1,"P":2});
+  });
+
+  it('should return amount of trapped water correctly 8', () => {
+    assert.deepEqual(makeExchange(7898), {"H":157,"Q":1,"D":2,"P":3});
+  });
+
+  it('should return amount of trapped water correctly 9', () => {
+    assert.deepEqual(makeExchange(8043), {"H":160,"Q":1,"D":1,"N":1,"P":3});
+  });
+
+  it('should return amount of trapped water correctly 10', () => {
+    assert.deepEqual(makeExchange(999), {"H":19,"Q":1,"D":2,"P":4});
+  });
+
+  it('should return amount of trapped water correctly 11', () => {
+    assert.deepEqual(makeExchange(-1000), {});
   });
 });
